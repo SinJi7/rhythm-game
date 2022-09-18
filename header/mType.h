69847552522de00e6be3760258c6c5c17630ff30
SinJi7 <93049777+SinJi7@users.noreply.github.com>
2022-09-18 15:36:33 +0900

    Add files via upload

---
+++ header/mType.h
@@ -0,0 +1,21 @@
+#ifndef MTYPE_h
+#define MTYPE_H
+
+namespace mType
+{
+    enum class sound
+    {
+    }; 
+    
+    enum class judgment_type
+    {
+        PERFECT = 1,
+        GOOD,
+        NICE,
+        BAD,
+        MISS,
+        NO
+    };
+}
+
+#endif
